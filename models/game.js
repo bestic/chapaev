250a3b21df425462a85c4dce362d43dd6e574a18
Joseph Chereshnovsky <jchereshnovsky@cogniance.com>
2013-11-02 21:08:47 +0200

    world is finally moved to separate nodejs module

--- models/game.js
+++ models/game.js
@@ -1,8 +1,14 @@
+var models = require('../models');
+World = models.World;
+
 var Game = function(players) {
   this.players = players;
+  this.world = null;
 
   this.init = function() {
-    console.log('Start game');
+      this.world = new World();
+      console.log('New game started');
+      console.log(this.world);
   };
 
   this.init();
