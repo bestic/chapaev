c2e77700bd6170a336ae62b63bf5fe285d7f21bd
Vladimir SHulga <vshulga@cogniance.com>
2013-11-02 18:06:11 +0200

    Add some interaction with socket io

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,7 +3,8 @@ var socket = io.connect();
 
 socket.on('message', function (data) {
   data = JSON.parse(data);
+  console.log('I got message from server');
   console.log(data);
 });
 
-socket.send(JSON.stringify({'data': 'Test data'}));
+socket.send(JSON.stringify({data: 'Test data', type: 'userMessage'}));
--- routes/sockets.js
+++ routes/sockets.js
@@ -8,9 +8,14 @@ exports.initialize = function(server) {
       type:'serverMessage',
       message: 'Welcome to the most interesting chat room on earth!'}
     ));
+
     socket.on('message', function(message){
+      console.log(message);
       message= JSON.parse(message);
+
+      console.log(message.type);
       if (message.type == "userMessage") {
+        console.log('This is user message');
         socket.broadcast.send(JSON.stringify(message));
         message.type = "myMessage";
         socket.send(JSON.stringify(message));
